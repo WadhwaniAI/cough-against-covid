203512687d1a4bfcfc6fed108cf5936488728d32
Piyush Bagad <piyush@wadhwaniai.org>
2021-03-19 00:35:08 +0530

    Adds default version file for cough clf with coswara data

---
+++ cac/utils/pandas.py
@@ -0,0 +1,22 @@
+"""Utility functions for pandas operations"""
+import numpy as np
+import pandas as pd
+
+
+def apply_filters(df: pd.DataFrame, filters: dict, reset_index=False):
+	"""
+	Filters df based on given filters (key-values pairs).
+	"""
+	X = df.copy()
+
+	for col, values in filters.items():
+		if isinstance(values, (list, tuple, np.ndarray)):
+			indices = X[col].isin(list(values))
+		else:
+			indices = X[col] == values
+		X = X[indices]
+
+	if reset_index:
+		X = X.reset_index(drop=True)
+
+	return X
