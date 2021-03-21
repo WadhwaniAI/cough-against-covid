437bf3c80a58c35ea017e3172610d8dd507d0f90
Piyush Bagad <piyush@wadhwaniai.org>
2021-03-21 17:01:21 +0530

    WIP

---
+++ datasets/download/freesound-kaggle.py
@@ -0,0 +1,9 @@
+"""
+Script to download and setup freesound dataset from Kaggle (https://www.kaggle.com/c/freesound-audio-tagging/data?select=train.csv)
+
+Instructions:
+
+1. Create Kaggle account and generate API key: In order to download this dataset,
+you need to setup Kaggle account and store your API key. Please follow instructions
+given here: https://github.com/Kaggle/kaggle-api
+"""
