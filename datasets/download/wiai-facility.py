30df64d3525f491a8c975f2ac5eeee4af2a06ef4
Piyush Bagad <piyush@wadhwaniai.org>
2021-03-29 23:18:47 +0530

    Adds script to download wiai-facility/ dataset

--- datasets/download/wiai-facility.py
+++ datasets/download/wiai-facility.py
@@ -5,9 +5,13 @@ multiple testing facilities across India.
 """
 import os
 from os.path import join, exists, basename
+from time import time
 from glob import glob
 from subprocess import call
 from termcolor import colored
+from tqdm import tqdm
+import py7zr
+import multivolumefile
 
 
 def create_folder(path):
@@ -20,33 +24,48 @@ DATASET_DIR = create_folder(join(DATA_DIR, 'wiai-facility'))
 # os.makedirs(DATASET_DIR, exist_ok=True)
 
 # <------------- Code to download .zip files --------------->
-# this shall be filled in when we have decided how to release
-# the dataset. Please ask code owners to supply the dataset
-# .zip file until it is offiicially released.
-# command: rsync -avzP piyushb@192.168.100.70:/scratche/data/cac/data/wiai-facility/wiai-facility-23-11-2020.zip ./
-# or  py7zr c -v 2000000k wiai-facility-23-11-2020.7z raw/
-zip_fpath = join(DATASET_DIR, 'wiai-facility-23-11-2020.zip')
-assert exists(zip_fpath), f"File {zip_fpath} not downloaded."
+"""
+This shall be filled in when we have decided how to release
+the dataset. Please ask code owners to supply the dataset
+.zip file(s) until it is offiicially released.
+
+(1) command: rsync -avzP piyushb@192.168.100.70:/scratche/data/cac/data/wiai-facility/wiai-facility-23-11-2020.zip ./
+(2) rsync -avzP piyushb@192.168.100.70:/scratche/data/cac/data/wiai-facility/wiai-facility-23-11-2020.7z* ./
+
+or  py7zr c -v 2000000k wiai-facility-23-11-2020.7z raw/ (takes about 40min per zip file.)
+It takes about 40mins to unzip.
+"""
+
+use_7z = True
+if use_7z:
+    # extracting without creating intermediate large file
+    zip_fpath = join(DATASET_DIR, 'wiai-facility-23-11-2020.7z')
+    unzip_folder = join(DATASET_DIR, 'raw/')
+    print(
+        colored(f"=> Unzipping .zip files: {zip_fpath} -> {unzip_folder}", 'yellow')
+    )
+    if not exists(unzip_folder):
+        start = time()
+        with multivolumefile.open(zip_fpath, mode='rb') as target_archive:
+            with py7zr.SevenZipFile(target_archive, 'r') as archive:
+                archive.extractall(path=DATASET_DIR)
+        end = time()
+        print(colored(f"Took {end - start} seconds.", 'white'))
+
+else:
+    # usual zip file not supported
+    raise NotImplementedError
+
 # <--------------------------------------------------------->
 
-unzip_folder = join(DATASET_DIR, basename(zip_fpath).split('.zip')[0])
-print(
-    colored(f"Unzipping .zip files: {zip_fpath} -> {unzip_folder}", 'yellow')
-)
-call(f"unzip '{zip_fpath}' -d {unzip_folder}/", shell=True)
+##### Note: the unzipped folder should already have the raw/ folder created.
+##### So there is no need to create raw/ folder and link it to source folder.
 
-# link raw/ folder to raw files
-raw_folder = create_folder(join(DATASET_DIR, 'raw'))
-# os.makedirs(raw_folder, exist_ok=True)
-print(
-    colored("Linking raw/ -> raw files.", 'yellow')
-)
-audio_folder = create_folder(join(raw_folder, 'audio'))
-src_audio_folders = glob(join(unzip_folder, 'raw/audio/patient_*'))
-tgt_audio_folders = [x.replace(unzip_folder, DATASET_DIR) for x in src_audio_folders]
+raw_folder = join(DATASET_DIR, 'raw')
+NUM_PATIENTS = 7169
+patient_folders = glob(join(raw_folder, 'audio/patient_*'))
+assert len(patient_folders) == NUM_PATIENTS
 
-annot_folder = create_folder(join(raw_folder, 'annotations'))
-src_annot_files = [join(unzip_folder, 'raw/annotations/CaC_label_sheet-final-nov23.csv')]
-tgt_annot_files = [x.replace(unzip_folder, DATASET_DIR) for x in src_annot_files]
-# os.makedirs(audio_folder, exist_ok=True)
 
+label_sheet = join(raw_folder, 'annotations/CaC_label_sheet-final-nov23.csv')
+assert exists(label_sheet)
