98ad7f6964390258e29b023db177a4984d8a3cfe
Piyush Bagad <piyush@wadhwaniai.org>
2021-04-22 15:17:03 +0530

    Adds NBs for dataset summary/analysis

--- cac/utils/viz.py
+++ cac/utils/viz.py
@@ -1,6 +1,27 @@
 from typing import List, Any
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as mc
+from matplotlib import rc
+import colorsys
+
+
+def lighten_color(color, amount=0.5):
+    """
+    Lightens the given color by multiplying (1-luminosity) by the given amount.
+    Input can be matplotlib color string, hex string, or RGB tuple.
+    Examples:
+    >> lighten_color('g', 0.3)
+    >> lighten_color('#F034A3', 0.6)
+    >> lighten_color((.3,.55,.1), 0.5)
+    """
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    return colorsys.hls_to_rgb(c[0], 1 - amount * (1 - c[1]), c[2])
 
 
 def plot_raw_audio_signal_with_markings(signal: np.ndarray, markings: list,
